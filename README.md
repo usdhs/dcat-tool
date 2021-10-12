17f4aabc079892636c27d51d204373223fd4fb21
Simson Garfinkel <simson.garfinkel@hq.dhs.gov>
2021-10-12 15:44:26 -0400

    Initial commit of a tool to find collection records

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # dcat-tool
+
+[DCAT](https://www.w3.org/TR/vocab-dcat-3/) is the Data Catalog Vocabulary, a product of the World Wide Web
+Consortium (WC3)'s Dataset Exchange Working Group.
+
+This repo contains a tool that is intended to have the following
+functionality:
+
+1. Given an RDF schema, generate an Excel spreadsheet that can be used
+  as a collection instrument for that schema.  (It's envisioned that
+  the RDF Schema will be a subset of DCAT, because we don't want to
+  collect with every attribute in the schema.)
+
+  The excel spreadsheet should contain minimal validation.
+
+2. Given a spreadsheet that's been filled out, parse the supplied data
+in the spreadsheet into its RDF representation.
+
+3. Given an RDF representation of data elements, validate them, and
+   create sensible error messages if validation fails. (We will use
+   this to create web-forms that allow people to upload either RDF
+   data or spreadsheets.)
+
+
+
+# For additional Reference
+
+These sites may be useful:
+
+* [rdflib documentation](https://rdflib.readthedocs.io/en/stable/)
+* [SHACL: Shapes Constraint Language, a W3C Recommendation](https://www.w3.org/TR/shacl/)
+* [OWL 2 Web Ontology Language Overview](https://www.w3.org/TR/owl2-overview/)
+* [W3C RDF 1.1 Primer](https://www.w3.org/TR/rdf11-primer/)
+
+## Related Concepts
+
+
+Turtle
+: An easy-to-author format for rendering RDF as text.
+
+RDF/XML
+: A rendering of RDF data into XML.
+
+JSON-LD
+: The JavaScript Object Notation Linked Data format, a specification
+: based on JSON.
---
+++ src/dcat_tool.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+List datasets and other information
+
+"""
+
+import os
+import os.path
+from os.path import abspath, dirname
+import sys
+import pprint
+from rdflib import Dataset, Graph, URIRef, Literal, Namespace
+
+COLLECT_TTL = os.path.join(dirname(abspath( __file__ )) , "../schemata/collect.ttl")
+
+
+if __name__=="__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Use a DCAT schema to produce capture instruments and APIs.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--schema", help="specify the collection schema file in Turtle, RDF/XML or RDF/JSON",
+                        default=COLLECT_TTL)
+    parser.add_argument("--debug", help="Enable debugging", action='store_true')
+    parser.add_argument("--print", help="Print the schema after it is read", action='store_true')
+    parser.add_argument("--write", help="write the schema to the specified file")
+    parser.add_argument("--makexls", help="specify the output filename of the Excel file to make for a collection schema")
+    args = parser.parse_args()
+
+    DHS = Namespace("http://github.com/usdhs/dcat-tool/0.1")
+    print("DHS:",DHS)
+
+
+    g = Graph()
+    g.parse(args.schema)
+    if args.print:
+        for stmt in g:
+            pprint.pprint(stmt)
+
+    if args.makexls:
+        for (s, p, o) in g.triples((None, None, DHS.CollectionRecord)):
+            print("s=",s,"p=",p,"o=",o)
+
+    if args.write:
+        fmt = os.path.splitext(args.write)[1][1:].lower()
+        if fmt=='json':
+            fmt='json-ld'
+        g.serialize(destination=args.write, format=fmt)
