45b39392761454dddb6e756d53db71883b50ec18
Simson Garfinkel <simson.garfinkel@hq.dhs.gov>
2021-11-04 09:38:36 -0400

    more refactoring

--- src/dcat_tool.py
+++ src/dcat_tool.py
@@ -20,15 +20,11 @@ import template_reader
 import dhs_ontology
 import easy_workbook
 
-RDFS          = Namespace("http://www.w3.org/2000/01/rdf-schema#")
-XSD           = Namespace("http://www.w3.org/2001/XMLSchema#")
 INSTRUCTIONS  = os.path.join(dirname(abspath( __file__ )) , "instructions.md")
-DEFAULT_WIDTH = 15              # Excel spreadsheet default width
-DEFAULT_TYPE  = XSD.string
 
-def make_template(fname, include_instructions, schema_dir=dhs_ontology.SCHEMATA_DIR, schema=dhs_ontology.COLLECT_TTL):
-    g = dhs_ontology.dcatv3_ontology(schema_dir, schema)
-    (g2, ci_objs) = get_template_column_info_objs(g, dhs_ontology.CI_QUERY)
+def make_template(fname, include_instructions, schemata_dir=dhs_ontology.SCHEMATA_DIR, schema=dhs_ontology.COLLECT_TTL, debug=False):
+    g = dhs_ontology.dcatv3_ontology(schemata_dir, schema)
+    (g2, ci_objs) = dhs_ontology.get_template_column_info_objs(g, dhs_ontology.CI_QUERY, debug=debug)
     eg = easy_workbook.ExcelGenerator()
     if include_instructions:
         eg.add_markdown_sheet("Instructions", open(INSTRUCTIONS).read())
@@ -40,93 +36,13 @@ def read_xlsx(fname) :
     return list(tr.inventory_records())
 
 
-class Simplifier:
-    def __init__(self, graph):
-        self.graph = graph
-    def simplify(self, token, namespace=True):
-        for prefix,ns in self.graph.namespaces():
-            if ns:
-                if token.startswith(ns):
-                    if namespace:
-                        return prefix+":"+token[len(ns):]
-                    else:
-                        return token[len(ns):]
-        return token
-
-def get_template_column_info_objs(g, query):
-    # g2 is an output graph of the terms in the collection instrument
-
-    g2 = Graph()
-
-    # Copy over the namespaces from the triples we read to the graph we are producing
-    for ns_prefix,namespace in g.namespaces():
-        g2.bind(ns_prefix, namespace)
-
-    ci_objs = []
-    simp = Simplifier(g)
-    for r in g.query( query ):
-        d = r.asdict()
-
-        if args.debug:
-            print(d)
-
-        if should_skip(d):
-            if args.debug:
-                print(">> skip")
-            continue
-
-        try:
-            title = d['aTitle']
-        except KeyError:
-            title = simp.simplify(d['aProperty'], namespace=False)
-
-        # For the comment, grab the shape comment if it is present. otherwise, grab the property comment.
-        # The comment goes into the tooltip for the column
-        comment = d.get('aShapeComment', d.get('aPropertyComment', ''))
-        if not comment:
-            print("Need description for",d['aProperty'])
-
-        obj = easy_workbook.ColumnInfo(value = title, # what is displayed in cell
-                                       comment = title + ":\n" + comment,
-                                       author = simp.simplify(d['aProperty']),
-                                       width = int(d.get('aWidth',DEFAULT_WIDTH)),
-                                       typ = simp.simplify(d.get('aType', DEFAULT_TYPE)),
-                                       group = d.get('aGroup',''),
-                                       )
-
-        # Add the object to the column list
-        ci_objs.append( obj )
-
-        # Now create the collection graph
-        try:
-            g2.add( (d['aProperty'], RDFS.range,   d['aType']) )
-        except KeyError as e:
-            pass
-
-        try:
-            g2.add( (d['aProperty'], RDFS.comment,   d['aComment']) )
-        except KeyError as e:
-            pass
-    return (g2, ci_objs)
-
-
-def should_skip(d):
-    """Skip query responses that are not in English"""
-    # Skip property comments that are not in english
-    try:
-        if d['aPropertyComment'].language != 'en':
-            return True
-    except (KeyError,AttributeError) as e:
-        pass
-    return False
-
 if __name__=="__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Use a DCAT schema to produce capture instruments and APIs.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--schema_dir",
-                        help="specify the directory to read all the schemata. If the --schema file is in schema_dir, it will only be read once.",
+    parser.add_argument("--schemata_dir",
+                        help="specify the directory to read all the schemata. If the --schema file is in schemata_dir, it will only be read once.",
                         default=dhs_ontology.SCHEMATA_DIR)
     parser.add_argument("--schema",
                         help="specify the collection schema file in Turtle, RDF/XML or RDF/JSON",
@@ -145,12 +61,12 @@ if __name__=="__main__":
 
     if args.dump:
         print("Dumping triple store:\n")
-        for stmt in sorted( dcatv3_ontology(args.schema_dir, args.schema) ):
+        for stmt in sorted( dcatv3_ontology(args.schemata_dir, args.schema) ):
             pprint.pprint(stmt)
             print()
 
     if args.validate:
-        v = dhs_ontology.Validator( args.schema_dir, args.schema )
+        v = dhs_ontology.Validator( args.schemata_dir, args.schema )
         while not sys.stdin.eof():
             jin  = json.load( sys.stdin )
             jout = validate( jin )
@@ -158,7 +74,7 @@ if __name__=="__main__":
         sys.stdout.write("\n")
 
     if args.make_template:
-        make_template(args.make_template, not args.noinstructions, schema_dir=args.schema_dir, schema=args.schema)
+        make_template(args.make_template, not args.noinstructions, schemata_dir=args.schemata_dir, schema=args.schema, debug=args.debug)
 
     if args.read_xlsx:
         for r in read_xlsx(args.read_xlsx):
--- src/dhs_ontology.py
+++ src/dhs_ontology.py
@@ -11,12 +11,18 @@ import glob
 import time
 import rdflib
 from rdflib import Dataset, Graph, URIRef, Literal, Namespace
-import openpyxl
+
 import template_reader
+import easy_workbook
 
 DHS           = Namespace("http://github.com/usdhs/dcat-tool/0.1")
+XSD           = Namespace("http://www.w3.org/2001/XMLSchema#")
+RDFS          = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+
 SCHEMATA_DIR  = os.path.join(dirname(abspath( __file__ )) , "../schemata")
 COLLECT_TTL   = os.path.join(SCHEMATA_DIR, "dhs_collect.ttl")
+DEFAULT_WIDTH = 15              # Excel spreadsheet default width
+DEFAULT_TYPE  = XSD.string
 
 """
 CI_QUERY is the query to create the collection instrument
@@ -34,19 +40,21 @@ WHERE {
   dhs:dataInventoryRecord sh:property ?aShapeName .
   ?aShapeName sh:path ?aProperty .
 
-  OPTIONAL { ?aProperty  rdfs:range     ?aType . }
-  OPTIONAL { ?aProperty  rdfs:comment   ?aPropertyComment . }
-  OPTIONAL { ?aShapeName dhs:excelWidth ?aWidth . }
-  OPTIONAL { ?aShapeName dt:title  ?aTitle . }
-  OPTIONAL { ?aShapeName dt:group  ?aGroup . }
+  OPTIONAL { ?aProperty  rdfs:range      ?aType . }
+  OPTIONAL { ?aProperty  rdfs:comment    ?aPropertyComment . }
+  OPTIONAL { ?aShapeName dhs:excelWidth  ?aWidth . }
+  OPTIONAL { ?aShapeName dt:title        ?aTitle . }
+  OPTIONAL { ?aShapeName dt:group        ?aGroup . }
   OPTIONAL { ?aShapeName rdfs:comment    ?aShapeComment . }
+  OPTIONAL { ?aShareName sh:minCount     ?aMinCount . }
 }
 """
 
-def dcatv3_ontology(schema_dir, schema_file):
+def dcatv3_ontology(schemata_dir = SCHEMATA_DIR, schema_file = COLLECT_TTL):
+    """Returns a graph of the DHS ontology for the data inventory program"""
     g    = Graph()
     seen = set()
-    for fname in glob.glob( os.path.join(schema_dir,"*.ttl")) + [schema_file]:
+    for fname in glob.glob( os.path.join(schemata_dir,"*.ttl")) + [schema_file]:
         if fname and fname not in seen:
             fname = os.path.abspath(fname)
             g.parse(fname)
@@ -55,14 +63,90 @@ def dcatv3_ontology(schema_dir, schema_file):
         raise RuntimeError("No schema files specified")
     return g
 
+class Simplifier:
+    def __init__(self, graph):
+        self.graph = graph
+    def simplify(self, token, namespace=True):
+        for prefix,ns in self.graph.namespaces():
+            if ns:
+                if token.startswith(ns):
+                    if namespace:
+                        return prefix+":"+token[len(ns):]
+                    else:
+                        return token[len(ns):]
+        return token
 
+def should_skip(d):
+    """Skip query responses that are not in English"""
+    # Skip property comments that are not in english
+    try:
+        if d['aPropertyComment'].language != 'en':
+            return True
+    except (KeyError,AttributeError) as e:
+        pass
+    return False
+
+def get_template_column_info_objs(g, query, debug=False):
+    # g2 is an output graph of the terms in the collection instrument
+
+    g2 = Graph()
+
+    # Copy over the namespaces from the triples we read to the graph we are producing
+    for ns_prefix,namespace in g.namespaces():
+        g2.bind(ns_prefix, namespace)
+
+    ci_objs = []
+    simp = Simplifier(g)
+    for r in g.query( query ):
+        d = r.asdict()
+
+        if debug:
+            print(d)
+
+        if should_skip(d):
+            if debug:
+                print(">> skip")
+            continue
+
+        try:
+            title = d['aTitle']
+        except KeyError:
+            title = simp.simplify(d['aProperty'], namespace=False)
+
+        # For the comment, grab the shape comment if it is present. otherwise, grab the property comment.
+        # The comment goes into the tooltip for the column
+        comment = d.get('aShapeComment', d.get('aPropertyComment', ''))
+        if not comment:
+            print("Need description for",d['aProperty'])
+
+        obj = easy_workbook.ColumnInfo(value = title, # what is displayed in cell
+                                       comment = title + ":\n" + comment,
+                                       author = simp.simplify(d['aProperty']),
+                                       width = int(d.get('aWidth',DEFAULT_WIDTH)),
+                                       typ = simp.simplify(d.get('aType', DEFAULT_TYPE)),
+                                       group = d.get('aGroup',''),
+                                       )
+
+        # Add the object to the column list
+        ci_objs.append( obj )
+
+        # Now create the collection graph
+        try:
+            g2.add( (d['aProperty'], RDFS.range,   d['aType']) )
+        except KeyError as e:
+            pass
+
+        try:
+            g2.add( (d['aProperty'], RDFS.comment,   d['aComment']) )
+        except KeyError as e:
+            pass
+    return (g2, ci_objs)
 
 class Validator:
-    def __init__(self, schema_dir, schema):
-        self.g = dcatv3_ontology(args.schema_dir, args.schema)
+    def __init__(self, schemata_dir = SCHEMATA_DIR, schema_file = COLLECT_TTL):
+        self.g = dcatv3_ontology(schemata_dir, schema_file)
         (self.g2, self.ci_objs) = get_template_column_info_objs(g, CI_QUERY)
         self.seenIDs = set()
-
 
     def add_row(self, row):
         """Validates a single object."""
